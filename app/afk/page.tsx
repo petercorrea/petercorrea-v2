32b701d1397b420f7218afad72c1b3b85c3aa8d8
Peter Correa <pmcorrea@me.com>
2024-01-08 16:21:26 -0500

    added theming functionality

---
+++ app/afk/page.tsx
@@ -0,0 +1,3 @@
+export default function AFK() {
+  return <div>AFK Page</div>;
+}
---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tailwindConfig: './tailwind.config.ts',
+  tailwindFunctions: ['clsx'],
+  tailwindFunctions: ['tw'],
+  plugins: ['prettier-plugin-tailwindcss'],
+};
