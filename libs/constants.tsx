4e20aa45edc2d42d221485090483ba4f067c15ae
Peter Correa <pmcorrea@me.com>
2024-02-18 11:46:18 -0500

    fixing build static assests

--- libs/constants.tsx
+++ libs/constants.tsx
@@ -6,7 +6,7 @@ export const routes = [
   { text: 'projects', href: '/#projects' },
   { text: 'posts', href: '/posts' },
   { text: 'afk', href: '/afk' },
-  { text: 'resume', href: '/Peter_Correa_Resume.pdf', download: 'true' },
+  { text: 'resume', href: '/Peter_Correa_resume.pdf', download: 'true' },
 ];
 
 export const postsMap = {
