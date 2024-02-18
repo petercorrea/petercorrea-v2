c4131956170aaed3ff890a21bc792188b8421a9a
Peter Correa <pmcorrea@me.com>
2024-02-17 22:01:24 -0500

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import Navbar from '@/components/nav/Navbar';
 import ModalProvider from '@/providers/ModalProvider';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -22,6 +24,8 @@ export default function RootLayout({
         <Navbar />
         <ModalProvider />
         {children}
+        <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
